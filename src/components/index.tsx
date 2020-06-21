c13b397e7d9c3776ab3c9279926c0abd9f49db4e
Simon Schröter <simon.schroeter@simonsystem.de>
2020-06-21 03:50:40 +0200

    refactore module index files

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 export default () => {
+  console.info('Rendering components');
   const elem = document.getElementById('app');
-  ReactDOM.render(<App />, elem, () => console.info(`Components rendered to ${elem}!`));
+  ReactDOM.render(<App />, elem, () => console.info('Components rendered to', elem));
 };
--- src/index.node.ts
+++ src/index.node.ts
@@ -1,8 +1,4 @@
-import dotenv from 'dotenv';
 import routes from './routes';
-
-// Load environment variable defaults from .env file
-dotenv.config();
 
 // Start and listen express routes
 routes();
